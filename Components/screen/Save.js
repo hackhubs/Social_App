21f56d4f30ef8c127fff5d1c092cac1c2129c3bb
hackhubs <abhav.thakur2018@vitstudent.ac.in>
2021-02-25 10:09:01 +0530

    storing inmage in store

--- Components/screen/Save.js
+++ Components/screen/Save.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Input, Image, Button } from "react-native-elements";
 
-const [caption, setCaption] = useState("");
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/firebase-storage";
 
 function Save(props) {
   console.log(props.route.params.image);
+  const [caption, setCaption] = useState("");
+  const UploadImage = async () => {
+    const uri = props.route.params.image;
+
+    const childPath = `post/${
+      firebase.auth().currentUser.uid
+    }/${Math.random().toString(36)}`;
+    console.log(childPath);
+
+    const response = await fetch(uri);
+
+    const blob = await response.blob(); //it will create the blob of uri which is pass along firestore then upload image
+
+    const task = firebase.storage().ref().child(childPath).put(blob); // it will upload the images to tthe storage
+
+    //to know the process of uploading the image
+
+    const taskProgress = (snapshot) => {
+      console.log(`transferred: ${snapshot.bytesTransferred}`); // to check how much is uploaded
+    };
+
+    const taskCompleted = () => {
+      task.snapshot.ref.getDownloadURL().then((snapshot) => {
+        //to get the download url which is public available to show the image uploaded
+        console.log(snapshot);
+      });
+    };
+
+    const taskError = (snapshot) => {
+      //to check the error
+      console.log(snapshot);
+    };
+
+    task.on("state_change", taskProgress, taskError, taskCompleted); // This should be called in proper order i.e progress => error => completed when the state changes these tasks get activated
+  };
 
   return (
     <View style={styles.container}>
@@ -14,7 +51,7 @@ function Save(props) {
         placeholder="Write a Caption..."
         onChangeText={(caption) => setCaption(caption)}
       />
-      <Button title="Save" />
+      <Button title="Save" onPress={() => UploadImage()} />
     </View>
   );
 }
